65f1db6d9154da5567ab78f8cb94b9a97f1689d6
Michael Fuerst <mail@michaelfuerst.de>
2017-10-05 23:46:05 +0200

    Improved solutions with a bit of documentation comments.

--- assignment3.py
+++ assignment3.py
@@ -1,18 +1,22 @@
 from time import sleep
 from robots.alice import Robot, ALICE_MAX_SPEED, ALICE_CENTER, ALICE_LEFT, ALICE_HALF_LEFT, ALICE_HALF_RIGHT, ALICE_RIGHT
 
-sleep(3)
-
-robot = Robot(host="192.168.23.1")
-#robot = Robot()
-
+ALICE_HARDWARE = False
 LENGTH = 1.0
 WIDTH = 0.5
 
+# Select if we need the hardware or not (localhost simulator).
+robot = None
+if ALICE_HARDWARE:
+    robot = Robot(host="192.168.23.1")
+else:
+    robot = Robot()
 
 def turn():
     robot.set_speed(ALICE_MAX_SPEED)
-    robot.set_turn(ALICE_RIGHT)
+    robot.set_turn(ALICE_LEFT)
+    # Value found empirically testing on the real alice robot
+    # Whereas the simulation is calibrated correctly this also works in simulation.
     sleep(0.8)
     robot.set_turn(ALICE_CENTER)
     robot.set_speed(0.0)
@@ -21,9 +25,10 @@ def turn():
 def straight(dist):
     robot.set_speed(ALICE_MAX_SPEED)
     robot.set_turn(ALICE_CENTER)
+    # Calculate the time we have to move to drive a certain distance.
     time = dist / ALICE_MAX_SPEED
     sleep(time)
-    robot.set_speed(0)
+    robot.set_speed(0.0)
 
 
 straight(LENGTH)
--- assignment4.py
+++ assignment4.py
@@ -1,15 +1,25 @@
 from robots.alice import Robot, ALICE_MAX_SPEED, ALICE_LEFT, ALICE_CENTER
 
-robot = Robot(host="192.168.23.1")
+ALICE_HARDWARE = False
+
+# Select if we need the hardware or not (localhost simulator).
+robot = None
+if ALICE_HARDWARE:
+    robot = Robot(host="192.168.23.1")
+else:
+    robot = Robot()
+
 robot.set_speed(ALICE_MAX_SPEED)
 
 
 # Let's be smart this time...
 def on_sensor_reading(sensor, data):
     if sensor == "distance/front":
+        # When we see nothing 0.0 should be mapped to max distance of 2.
         data = [2.0 if d <= 0.0 else d for d in data]
-        #print(data)
         min_dist = min(data)
+
+        # If there is something, just turn left
         if min_dist < 0.2:
             robot.set_turn(ALICE_LEFT)  # Use all speed to turn as sharp as possible
         else:
