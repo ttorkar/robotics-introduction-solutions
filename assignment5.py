c8a4a1899f22d44c39f81f19a25809bd6813e790
Anton Schirg <anton.schirg@posteo.de>
2017-10-05 21:08:43 +0200

    assignment 5 compass control

---
+++ assignment5.py
@@ -0,0 +1,43 @@
+from robots.alice import Robot
+from math import pi
+import time
+
+desired_direction = -1.57
+integrator = 0
+last_time = None
+
+
+def on_global_position(lat, lon, angle_to_north, accuracy):
+    global desired_direction, integrator, last_time
+
+    print "latlon: " + str(lat) + ":" + str(lon)
+
+    dt = None
+    if last_time is None:
+        last_time = time.time()
+        return
+    else:
+        dt = time.time() - last_time
+        last_time = time.time()
+
+    error = (-angle_to_north) - desired_direction
+    error %= 2*pi
+    if error > pi:
+        error -= 2*pi
+
+    print "ERR", error
+
+    integrator += error * dt
+
+    control = error * 3.0# + integrator * 0.5
+
+    robot.set_turn(-control)
+
+    print "AN", angle_to_north
+
+robot = Robot()
+robot.on_global_position = on_global_position
+robot.set_speed(0.1)  # move with 1 m/s
+
+robot.wait()
+robot.shutdown()
