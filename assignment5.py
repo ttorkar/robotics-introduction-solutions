96301b73efb72a9a6d6ed108c8e37026350d9e41
Trent Torkar <30887582+ttorkar@users.noreply.github.com>
2017-10-06 19:27:50 +1100

    Fixed desired_direction, made more concise
    Fixed radians, made more concise. Removed negative radians and changed to work on 0 - 6.28319 spectrum.

--- assignment5.py
+++ assignment5.py
@@ -1,39 +1,26 @@
 from robots.alice import Robot
 from math import pi
-import time
 
-desired_direction = -1.57
+
+#Radians
+desired_direction = 1.57*3
 integrator = 0
 last_time = None
 
-
-def on_global_position(lat, lon, angle_to_north, accuracy):
-    global desired_direction, integrator, last_time
-
-    print "latlon: " + str(lat) + ":" + str(lon)
-
-    dt = None
-    if last_time is None:
-        last_time = time.time()
-        return
-    else:
-        dt = time.time() - last_time
-        last_time = time.time()
-
-    error = (-angle_to_north) - desired_direction
+def direction(angle_to_north):
+    error = (angle_to_north) - desired_direction
     error %= 2*pi
     if error > pi:
         error -= 2*pi
 
-    print "ERR", error
-
-    integrator += error * dt
-
-    control = error * 3.0# + integrator * 0.5
-
-    robot.set_turn(-control)
-
-    print "AN", angle_to_north
+    control = error * 3.0
+    print("Error: ",error)
+    print('Degrees:{}, Radians:{}'.format(int(control*180/pi),control))
+    return control
+    
+def on_global_position(lat, lon, angle_to_north, accuracy):
+    robot.set_turn(direction(angle_to_north))
+    
 
 robot = Robot()
 robot.on_global_position = on_global_position
